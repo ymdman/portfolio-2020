ba3fc76824f7f5997722f3954b542db5ba78d83c
ymdman <revitalized@icloud.com>
2019-12-19 21:53:54 +0900

    Style: Top page hero

--- src/pages/PfoTop/index.vue
+++ src/pages/PfoTop/index.vue
@@ -1,6 +1,16 @@
 <template>
-  <div :style="{ height: `${windowHeight}px` }" class="hero">
-    {{ windowHeight }}
+  <div>
+    <div class="hero">
+      {{ windowHeight }}
+    </div>
+    <div class="content">
+      <h2 class="title">About</h2>
+      <p>
+        Lorem, ipsum dolor sit amet consectetur adipisicing elit. Fuga, quaerat!
+      </p>
+      <h3>Point</h3>
+      <h3>Profile</h3>
+    </div>
   </div>
 </template>
 
@@ -9,3 +19,8 @@
   import VueComponent from './index';
   export default VueComponent;
 </script>
+
+<!-- prettier-ignore -->
+<style scoped>
+  @import './style';
+</style>
