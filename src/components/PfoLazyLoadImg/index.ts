8203de54aed973d0ce7e0bdcf8b29d7994aef552
ymdman <revitalized@icloud.com>
2019-12-19 22:11:46 +0900

    Feat: Add LazyLoad component

---
+++ src/components/PfoLazyLoadImg/index.ts
@@ -0,0 +1,39 @@
+import { Component, Prop, Vue } from 'vue-property-decorator';
+
+@Component
+export default class PfoLazyLoadImg extends Vue {
+  @Prop({ type: String, required: true })
+  src!: string;
+
+  isLoadingSupported = 'loading' in HTMLImageElement.prototype;
+  io = {} as IntersectionObserver;
+
+  mounted(): void {
+    if (!this.isLoadingSupported) {
+      this.observe();
+    }
+  }
+
+  destroyed(): void {
+    if (!this.isLoadingSupported) {
+      this.unobserve();
+    }
+  }
+
+  observe(): void {
+    this.io = new IntersectionObserver(entries => {
+      const entry = entries[0];
+      if (!entry.isIntersecting) {
+        return;
+      }
+      this.unobserve();
+      this.$el.setAttribute('src', this.src);
+    });
+    this.io.observe(this.$el);
+  }
+
+  unobserve(): void {
+    this.io.unobserve(this.$el);
+    this.io.disconnect();
+  }
+}
---
+++ src/components/PfoLazyLoadImg/index.vue
@@ -0,0 +1,19 @@
+<template>
+  <img
+    :src="isLoadingSupported && src"
+    loading="lazy"
+    width="500"
+    height="1000"
+  />
+</template>
+
+<!-- prettier-ignore -->
+<script lang="ts">
+  import VueComponent from './index';
+  export default VueComponent;
+</script>
+
+<!-- prettier-ignore -->
+<style scoped>
+  @import './style';
+</style>
