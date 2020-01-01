48d3ef2aeac9080c83cd52579eee216a3ea2e545
ymdman <revitalized@icloud.com>
2020-01-01 18:24:29 +0900

    Refactor: Adjust Overall layout

--- src/pages/Works/index.vue
+++ src/pages/Works/index.vue
@@ -1,14 +1,18 @@
 <template>
-  <div class="l-container">
-    <h1 class="c-title c-title--level-1">Works</h1>
-    <p>{{ description }}</p>
-    <div class="l-content">
-      <section
-        v-for="item in list"
-        :key="item.key"
-        class="l-section l-section--lined"
-      >
-        <h2 class="c-title c-title--level-2">{{ item.companyName }}</h2>
+  <div class="l-content">
+    <div class="l-content__section">
+      <h1 class="c-title c-title--level-1">Works</h1>
+      <div class="l-content__body">
+        <p>{{ description }}</p>
+      </div>
+    </div>
+    <section
+      v-for="item in list"
+      :key="item.key"
+      class="l-content__section l-content__section--lined"
+    >
+      <h2 class="c-title c-title--level-2">{{ item.companyName }}</h2>
+      <div class="l-content__body">
         <dl class="summary-list">
           <dt class="summary-list__title">Job / 職種</dt>
           <dd class="summary-list__label">{{ item.job }}</dd>
@@ -18,31 +22,31 @@
           <dd class="summary-list__label">{{ item.period }}</dd>
         </dl>
         <p class="description">{{ item.description }}</p>
-        <div class="project">
-          <ul class="project__list">
-            <li
-              v-for="project in item.projects"
-              :key="project.key"
-              class="project__item"
-            >
-              <figure class="project__detail">
-                <div class="project__image">
-                  <v-lazy-load-img
-                    :src="project.image.src"
-                    :alt="project.image.alt"
-                    :width="600"
-                    :height="400"
-                  />
-                </div>
-                <figcaption class="project__caption">
-                  {{ project.siteName }}
-                </figcaption>
-              </figure>
-            </li>
-          </ul>
-        </div>
-      </section>
-    </div>
+      </div>
+      <div class="project">
+        <ul class="project__list">
+          <li
+            v-for="project in item.projects"
+            :key="project.key"
+            class="project__item"
+          >
+            <figure class="project__detail">
+              <div class="project__image">
+                <v-lazy-load-img
+                  :src="project.image.src"
+                  :alt="project.image.alt"
+                  :width="600"
+                  :height="400"
+                />
+              </div>
+              <figcaption class="project__caption">
+                {{ project.siteName }}
+              </figcaption>
+            </figure>
+          </li>
+        </ul>
+      </div>
+    </section>
   </div>
 </template>
 
