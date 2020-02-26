2d2469e4e8914ce217b53cd6dbdac6de94bd8ae8
ymdman <revitalized@icloud.com>
2020-02-26 23:08:24 +0900

    Refctor: Fix not found page

--- src/pages/NotFound/index.vue
+++ src/pages/NotFound/index.vue
@@ -1,9 +1,11 @@
 <template>
-  <div class="l-content">
-    <div class="l-content__section l-content__section--centered">
-      <h1 class="c-title c-title--level-1">404 Not Found</h1>
-      <div class="l-content__body">
-        <p>お探しのページが見つかりませんでした。</p>
+  <div class="not-found">
+    <div class="l-content">
+      <div class="l-content__section l-content__section--centered">
+        <h1 class="c-title c-title--level-1">404 Not Found</h1>
+        <div class="l-content__body">
+          <p>お探しのページが見つかりませんでした。</p>
+        </div>
       </div>
     </div>
   </div>
@@ -14,3 +16,8 @@
   import VueComponent from './index';
   export default VueComponent;
 </script>
+
+<!-- prettier-ignore -->
+<style scoped>
+  @import './style';
+</style>
