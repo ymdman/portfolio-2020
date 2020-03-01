82a1785a72b31baeccd4dac20ac8869099b60297
ymdman <revitalized@icloud.com>
2020-03-01 16:51:34 +0900

    Rrefactor: Fix thumbnail in top page

--- src/pages/Works/index.vue
+++ src/pages/Works/index.vue
@@ -36,17 +36,15 @@
                 >
                   <figure class="project__figure">
                     <div class="project__thumbnail">
-                      <div class="project__thumbnail-inner">
-                        <v-lazy-load-img
-                          v-if="!!project.image"
-                          :src="project.image.src"
-                          :alt="project.image.alt"
-                          :width="600"
-                          :height="400"
-                          class="project__image"
-                        />
-                        <div v-else class="project__no-image">No Image</div>
-                      </div>
+                      <v-lazy-load-img
+                        v-if="!!project.image"
+                        :src="project.image.src"
+                        :alt="project.image.alt"
+                        :width="600"
+                        :height="400"
+                        class="project__image"
+                      />
+                      <div v-else class="project__no-image">No Image</div>
                     </div>
                     <figcaption class="project__caption">
                       {{ project.siteName }}
