b0147488f4e22977742ea85716f73361e4544c75
ymdman <revitalized@icloud.com>
2020-03-14 00:57:13 +0900

    Feat: Add CSS to 404 page

--- src/pages/NotFound/index.vue
+++ src/pages/NotFound/index.vue
@@ -3,7 +3,7 @@
     <div class="l-content__section l-content__section--centered">
       <h1 class="c-title c-title--level-1">404 Not Found</h1>
       <div class="l-content__body">
-        <p>お探しのページが見つかりませんでした。</p>
+        <p class="description">お探しのページが見つかりませんでした。</p>
       </div>
     </div>
   </div>
@@ -14,3 +14,8 @@
   import VueComponent from './index';
   export default VueComponent;
 </script>
+
+<!-- prettier-ignore -->
+<style scoped>
+  @import './index';
+</style>
